0bb0ba8720522afd0cd244fefa00755837afa14d
Kayky <rodrigueskayky2025@gmail.com>
2026-05-13 16:44:12 -0300

    Nome final da api

--- src/components/SaleForm.jsx
+++ src/components/SaleForm.jsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import api from "../services/Api";
+import api from "../services/api";
 import { useNavigate } from "react-router-dom";
 
 export default function SaleForm() {
